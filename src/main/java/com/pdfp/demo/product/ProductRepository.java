4f78125b293fd81fe054779f86696f9e4ed1f6a8
Pedro Fernández <pdf3rn@gmail.com>
2025-07-22 13:54:49 -0300

    fix: code errors

--- src/main/java/com/pdfp/demo/product/ProductRepository.java
+++ src/main/java/com/pdfp/demo/product/ProductRepository.java
@@ -2,7 +2,5 @@ package com.pdfp.demo.product;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.UUID;
-
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
