69a36e01e52e0ffa2c19669b964f0926b95316db
Pedro Fernández <pdf3rn@gmail.com>
2025-07-22 16:46:24 -0300

    update: frontend app

--- src/main/java/com/pdfp/demo/product/dto/ProductRequest.java
+++ src/main/java/com/pdfp/demo/product/dto/ProductRequest.java
@@ -4,11 +4,12 @@ import jakarta.persistence.Column;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import java.math.BigDecimal;
 
 public record ProductRequest(
         @NotBlank @Column(unique = true) String name,
         @DecimalMin("1.00") BigDecimal price,
-        @Min(0) Integer stock
+        @NotNull @Min(0) Integer stock
 ) {}
